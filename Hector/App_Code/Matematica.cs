19ac70758ce03a464aad89d3c616ff2eabafb9d3
cescaba <cescaba_10@hotmail.com>
2015-03-30 23:29:38 -0500

    1.4
    4Hector changes

---
+++ Hector/App_Code/Matematica.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+class Matematica
+{
+    private int num1;
+    public int Num1
+    {
+        get
+        {
+            return num1;
+        }
+        set
+        {
+            num1 = value;
+        }
+    }
+
+    private int num2;
+    public int Num2
+    {
+        get
+        {
+            return num2;
+        }
+        set
+        {
+            num2 = value;
+        }
+    }
+
+    private string suma;
+    public string Suma
+    {
+        get
+        {
+            return suma;
+        }
+        set
+        {
+            suma = value;
+        }
+    }
+
+    private string resta;
+    public string Resta
+    {
+        get
+        {
+            return resta;
+        }
+        set
+        {
+            resta = value;
+        }
+    }
+
+    private string multiplicacion;
+    public string Multiplicacion
+    {
+        get
+        {
+            return multiplicacion;
+        }
+        set
+        {
+            multiplicacion = value;
+        }
+    }
+
+    private string division;
+    public string Division
+    {
+        get
+        {
+            return division;
+        }
+        set
+        {
+            division = value;
+        }
+    }
+
+
+    
+
+    public Matematica()
+	{
+        
+	}
+
+    
+
+}
